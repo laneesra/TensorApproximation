f4b5540d6224a15493c4b0ebe7fa404fb28f6cd0
laneesra <shishi_98@mail.ru>
2020-05-29 16:03:52 +0300

    added install files

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 mkdir TN_venv;
 mkdir CNNs/models;
-<<<<<<< HEAD
-=======
 mkdir CNNs/logs;
->>>>>>> 9a722e26fec951a038ce74d4158c59d8801f9730
+
 python3 -m venv TN_venv && source TN_venv/bin/activate && pip3 install -r requirements.txt;
 
 
